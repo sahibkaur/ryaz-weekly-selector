3ab04a08058e0ebc255b61ed3fd6876d6b051e20
Sahibpreet Kaur <sahib@ishwerdas.com>
2019-06-30 18:40:11 +0530

    add html css and js :P

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// window.onclick = getData;
+
+function getData(){
+	var names = ["Sahib", "Shaina", "Vigas", "Panda", "Mandeep", "Varmillion", "Manpreet", "Hemant"];
+	var dynNames=names;
+	var length = names.length;
+	var dynLength = length;
+	var cell = document.getElementsByClassName("cell");
+	for (var i=0; i< dynNames.length;i++){
+		console.log(dynNames[i]);
+	}
+	for( var i = 0; i < length; i++){
+		random = Math.floor(Math.random() * dynLength);
+		cell[i].innerHTML = dynNames[random];
+		// for( var j = random; j < dynLength; j++){
+		// 	dynNames[j] = dynNames[j+1];
+		// }
+		dynNames.splice(random,1);
+		dynLength--;
+		console.log(dynLength);
+	}
+}
